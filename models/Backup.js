68ee9bf380ecde9c0861377686648ed2fcbf5773
denisreeves <techwrk.mmglobus@gmail.com>
2025-04-07 18:17:58 +0530

    Delete models directory

--- models/Backup.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const BackupSchema = new mongoose.Schema({
-  userId: { type: String, required: false },
-  filePath: { type: String, required: false },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Backup", BackupSchema);
